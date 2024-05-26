13d594c1d6f04df624cd63425e5249794f8d4293
dariusz.piwowarski <d.piwowarski.mail@gmail.com>
2024-05-26 15:40:28 +0200

    script for building and pushing to docker hub

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Example App
 
-## Running
+## Building and running locally
 ```
 cd src
 docker build -t example-app .
 docker run -p 8080:8080 example-app
+```
+
+## Building and pushing to docker hub
+```
+chmod u+x build_and_push.sh
+./build_and_push.sh
 ```
---
+++ build_and_push.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Define variables
+DOCKER_USERNAME="suuargo"
+IMAGE_NAME="example-app"
+TAG="latest"
+
+set -e
+
+echo "Logging in to Docker Hub..."
+docker login
+
+# Navigate to the directory containing the Dockerfile
+# Adjust the path as needed
+cd "$(dirname "$0")/src"
+
+# Build the Docker image
+echo "Building Docker image..."
+docker build -t ${IMAGE_NAME} .
+
+# Tag the image for Docker Hub
+echo "Tagging Docker image..."
+docker tag ${IMAGE_NAME} ${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG}
+
+# Push the image to Docker Hub
+echo "Pushing Docker image to Docker Hub..."
+docker push ${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG}
+
+echo "Docker image pushed successfully!"
